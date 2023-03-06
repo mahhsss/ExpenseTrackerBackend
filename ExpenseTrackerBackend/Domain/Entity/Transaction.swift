53b7854153de8f168631c368c7e83d099c79a7d7
Mahesh <19tuit047@skct.edu.in>
2023-03-06 14:09:35 +0530

    Seventh Commit Modified

--- ExpenseTrackerBackend/Domain/Entity/Transaction.swift
+++ ExpenseTrackerBackend/Domain/Entity/Transaction.swift
@@ -25,11 +25,11 @@ public struct Transaction {
     var transactionType: TransactionType
     var currencyType: CurrencyType
     var date: String
-    var category: String
+    var category: String?
     var note: String?
     
     
-    public init(transactionId: Int, userId: Int, amount: Int, transactionType: TransactionType, currencyType: CurrencyType, date: String, category: String, note: String? = nil) {
+    public init(transactionId: Int, userId: Int, amount: Int, transactionType: TransactionType, currencyType: CurrencyType, date: String, category: String? = nil, note: String? = nil) {
         self.transactionId = transactionId
         self.userId = userId
         self.amount = amount
